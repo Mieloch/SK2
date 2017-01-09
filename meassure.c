7953fbb5f354672f011820a4806c747814f00c16
Mieloch <pawel.mieloch@gmail.com>
2017-01-09 19:42:03 +0100

    init

---
+++ meassure.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int cpu_usage(void) {
+	FILE *pp;
+	pp = popen("top -b -n 1 | tail -n +8 | awk '{ sum += $9 } END {print sum }'", "r");
+	if (pp != NULL) {
+		char *line;
+		char buf[1000];
+		line = fgets(buf,sizeof buf, pp);
+		if(line == NULL){
+			return -1;
+		}
+		int cpu_usage = atoi(line);
+		return cpu_usage;
+		
+	}	
+	return -1;
+}
+
+int main(void){
+printf("cpu usage = %d%%\n", cpu_usage());
+}
