1032f23f1ed341a779bad9e46ddac9021e177b4c
miits <mikolaj.szal@gmail.com>
2017-01-12 18:44:52 +0100

    multiprocess server

--- server.c
+++ server.c
@@ -17,7 +17,8 @@
 #define SERVER_PORT 1234
 #define QUEUE_SIZE 5
 
-void run_python_hello_world(char *name){
+void run_python_hello_world(char *name)
+{
 	wchar_t *program = Py_DecodeLocale(name, NULL);
 	if(program == NULL){
 		fprintf(stderr, "Fatal error: cannot decode name\n");
@@ -30,12 +31,14 @@ void run_python_hello_world(char *name){
 	PyMem_RawFree(program);
 }
 
-void handle_cpu_usage_request(int clientSocket){
+void handle_cpu_usage_request(int clientSocket)
+{
 	int cpu_usage = get_cpu_usage();
 	write(clientSocket, &cpu_usage, sizeof(int));
 }
 
-void handle_execute_job_request(int clientSocket, char* script){
+void handle_execute_job_request(int clientSocket, char* script)
+{
 	printf("EXECUTE_JOB\n");
 
 	wchar_t *program = Py_DecodeLocale("server", NULL);
@@ -54,82 +57,103 @@ void handle_execute_job_request(int clientSocket, char* script){
 
 int reverse(int num)
 {
-return ((num>>24)&0xff) | // move byte 3 to byte 0
+	return ((num>>24)&0xff) | // move byte 3 to byte 0
                     ((num<<8)&0xff0000) | // move byte 1 to byte 2
                     ((num>>8)&0xff00) | // move byte 2 to byte 1
                     ((num<<24)&0xff000000);
 }
 
-void process_request(int clientSocket){
-           struct client_request *request = malloc(sizeof(struct client_request));
+void process_request(int clientSocket)
+{
+	struct client_request *request = malloc(sizeof(struct client_request));
 
-           read(clientSocket, request, (sizeof(struct client_request)));
-	   request->code = reverse(request->code);
+	read(clientSocket, request, (sizeof(struct client_request)));
+	request->code = reverse(request->code);
 
 
 
-	   if(request->code == GET_CPU_USAGE){
+	if(request->code == GET_CPU_USAGE)
+	{
 		handle_cpu_usage_request(clientSocket);
-	   }else if(request->code == EXECUTE_JOB){
-	   	printf("script payload = %s\n", request->payload);  	   
+	}
+	else if(request->code == EXECUTE_JOB)
+	{
+		printf("script payload = %s\n", request->payload);  	   
 		handle_execute_job_request(clientSocket,request->payload);
-	   }
+	}
 }
 
 int main(int argc, char* argv[])
 {
-   int nSocket, nClientSocket;
-   int nBind, nListen;
-   int nFoo = 1;
-   socklen_t nTmp;
-   struct sockaddr_in stAddr, stClientAddr;
+	int nSocket, nClientSocket;
+	int nBind, nListen;
+	int nFoo = 1;
+	int pid;
+	socklen_t nTmp;
+	struct sockaddr_in stAddr, stClientAddr;
 
 
-   /* address structure */
-   memset(&stAddr, 0, sizeof(struct sockaddr));
-   stAddr.sin_family = AF_INET;
-   stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   stAddr.sin_port = htons(SERVER_PORT);
+	/* address structure */
+	memset(&stAddr, 0, sizeof(struct sockaddr));
+	stAddr.sin_family = AF_INET;
+	stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	stAddr.sin_port = htons(SERVER_PORT);
 
-   /* create a socket */
-   nSocket = socket(AF_INET, SOCK_STREAM, 0);
-   if (nSocket < 0)
-   {
-       fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
-       exit(1);
-   }
-   setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
+	/* create a socket */
+	nSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (nSocket < 0)
+	{
+		fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
+		exit(1);
+	}
+	setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
 
-   /* bind a name to a socket */
-   nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
-   if (nBind < 0)
-   {
-       fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
-       exit(1);
-   }
-   /* specify queue size */
-   nListen = listen(nSocket, QUEUE_SIZE);
-   if (nListen < 0)
-   {
-       fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
-   }
+	/* bind a name to a socket */
+	nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
+	if (nBind < 0)
+	{
+		fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
+		exit(1);
+	}
+	/* specify queue size */
+	nListen = listen(nSocket, QUEUE_SIZE);
+	if (nListen < 0)
+	{
+		fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
+	}
 
 	printf("Server started\n");
-   while(1)
-   {
-       /* block for connection request */
-       nTmp = sizeof(struct sockaddr);
-       nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
-       if (nClientSocket < 0)
-       {
-           fprintf(stderr, "%s: Can't create a connection's socket.\n", argv[0]);
-           exit(1);
-       }
-      // printf("%s: [connection from %s]\n", argv[0], inet_ntoa((struct in_addr)stClientAddr.sin_addr));
-       process_request(nClientSocket);
-       close(nClientSocket);
-   }
 
-   close(nSocket);
-   return(0);
+	while(1)
+	{
+		/* block for connection request */
+		nTmp = sizeof(struct sockaddr);
+		nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
+		if (nClientSocket < 0)
+		{
+			fprintf(stderr, "%s: Can't create a connection's socket.\n", argv[0]);
+			exit(1);
+		}
+		//printf("%s: [connection from %s]\n", argv[0], inet_ntoa((struct in_addr)stClientAddr.sin_addr));
+		pid = fork();
+		if(pid < 0)
+		{
+			close(nClientSocket);
+			fprintf(stderr, "%s: Can't create process.\n", argv[0]);
+			exit(1);
+		}
+
+		if(pid == 0)
+		{
+			process_request(nClientSocket);
+			close(nClientSocket);
+		}
+		else if(pid > 0)
+		{
+			close(nClientSocket);
+		}
+		
+	}
+	close(nSocket);
+	return(0);
 }
